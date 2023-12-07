09091ca566efb97e760451431ee545ff00988c27
Ionut Ciovica <ionut.ciovica@gmail.com>
2023-12-07 15:13:19 +0200

    Initial Commit

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by IonutCiovica on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
